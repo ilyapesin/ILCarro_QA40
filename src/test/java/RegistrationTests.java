674df74578a9fa04c30ba7bdf7dbc802d9c57d5e
ilyapesin <pesinilya@gmail.com>
2023-11-16 22:11:55 +0200

    CW8

--- src/test/java/RegistrationTests.java
+++ src/test/java/RegistrationTests.java
@@ -5,7 +5,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class RegistrationTest extends TestBase{
+public class RegistrationTests extends TestBase{
     @BeforeMethod
     public void preconditions() {
         if (app.getUser().isLogged()) {
